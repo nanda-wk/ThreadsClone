1aeb5bc1ea7bce2ddccf1824a2ca01b1dc9ceab6
Nanda WK <nandawinkyu.ix@gmail.com>
2024-02-10 13:35:37 +0630

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Nanda WK on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
