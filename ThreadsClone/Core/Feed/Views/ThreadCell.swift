eaf185b4a2c1ce0860705d57e6d6ec5e972dd57d
Nanda WK <nandawinkyu.ix@gmail.com>
2024-02-13 22:21:56 +0630

    feed view UI development

---
+++ ThreadsClone/Core/Feed/Views/ThreadCell.swift
@@ -0,0 +1,82 @@
+//
+//  ThreadCell.swift
+//  ThreadsClone
+//
+//  Created by Nanda WK on 13/02/2024.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack (alignment: .top, spacing: 12) {
+                Image(.lichKing)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("lichking_1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        }
+                    }
+                        
+                    Text("King of Undead in World of Warcraft.")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
